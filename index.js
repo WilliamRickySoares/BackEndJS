aac83b25d432e63f3f6f7994579bc739144fb7cc
William Ricky Soares <89048945+WilliamRickySoares@users.noreply.github.com>
2024-02-20 14:42:54 +0000

    Instalação do npm express

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("Hello World");
-console.log("Meu primeiro script Node.JS")
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World - API WRCSOARES')
+})
+
+app.listen(3000)
