aadc4df2e7e1d042571d4748d024b32db67e6c00
William Ricky Soares <89048945+WilliamRickySoares@users.noreply.github.com>
2024-02-20 15:05:42 +0000

    Criação do endpoint /oi

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World - API WRCSOARES')
 })
 
+app.get('/oi', function (req, res) {
+    res.send('Olá mundo, nova rota de /oi')
+  })
+
 app.listen(3000)
